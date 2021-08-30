8ef49086601f626965e1275bd54588e459151aeb
sajanasaji1304 <sajanasaji13@gmail.com>
2021-08-30 14:28:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Another from './components/Another';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('')
+  function click(){
+    setName('SAJANA')
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>helo {name}</h1>
+      <button onClick={click}>click</button>
+<Another/>
+
     </div>
   );
 }
---
+++ src/components/Another.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from '../styles/Another.module.css'
+
+function Another() {
+    return (
+        <div>
+            <h1 style={{color:"red", fontSize:"30px"}} >sajna</h1>
+            <h1  className={classes.text}>sajna</h1>
+            
+        </div>
+    )
+}
+
+export default Another
